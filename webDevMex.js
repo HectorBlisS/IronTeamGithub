d141795d3f45a840345470edf155f18825fe462d
Davinia Roca <daviniaroca@Davinias-MacBook-Air.local>
2018-02-19 17:21:42 -0600

    last last one left solving conflicts

--- webDevMex.js
+++ webDevMex.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+dia
 nochet
-Traigan tributos vasallos mios!! ¡Orale!
-=======
 Traigan tributos vasallos mios!! ¡Orale!
 Hola
 Davinia
->>>>>>> 0de5a03f72e3f14ba8a202751f0eabd998f79e03
